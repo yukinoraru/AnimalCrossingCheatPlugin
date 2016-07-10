a038afbf9db1d17609d54015c2024c17d435404e
yukinoraru <yukinoraru@gmail.com>
2016-07-10 18:00:36 +0900

    implements pullAllWeeds

--- source/acnl_cheats.c
+++ source/acnl_cheats.c
@@ -1,4 +1,4 @@
-#include "helpers.h"
+#include "acnl_cheats.h"
 
 //*********************************
 // ITEM SECTION
@@ -17,7 +17,34 @@ void duplicateItem(){
 
 //
 void pullAllWeeds(){
+  // define weeds
+  u32 weeds[] = {
+    0x0000007C, // Weed 1
+    0x0000007D, // Weed 2
+    0x0000007E, // Weed 3
+    0x0000007F, // Weed 4
+    0x000000CB, // Dandelion Puff
+    0x000000CC, // Clovers (Weed)
+    0x000000CD, // Rafflesia
+    0x000000F8, // Rafflesia (Wilted)
+  };
 
+  int i;
+  u32 address, item;
+
+  // find all items in Town
+  for(address = OFFSET_TOWN_ITEMS; address < OFFSET_TOWN_ITEMS + RANGE_TOWN_ITEMS; address += ITEM_BYTES){
+    item = READU32(address);
+
+    // find weeds
+    for(i = 0; i < sizeof(weeds) / sizeof(u32); i++){
+      if(item == weeds[i]){
+        // replace weeds to nothing(=0x7FFE)
+        WRITEU32(address, 0x00007FFE);
+        break;
+      }
+    }
+  }
 }
 
 //
--- source/acnl_cheats.h
+++ source/acnl_cheats.h
@@ -14,4 +14,9 @@ void growAllTrees();
 // PLAYER
 void setPlayerSpeedFaster();
 
+// DEFINE CONSTANTS
+#define OFFSET_TOWN_ITEMS 0x1609D158
+#define ITEM_BYTES 0x04
+#define RANGE_TOWN_ITEMS 0x5000
+
 #endif
